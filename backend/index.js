16ce1eebed6cdde177816fab5413fd9522bac146
Md Shahid Ali <shahidali39374@gmail.com>
2025-11-07 04:02:19 +0530

    Modified the code in the db connection for check the MONGO_URI is set or not

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import notesrouter from './src/Router/notesrouter.js'
 
 const app = express();
 dotenv.config();
-app.use(cors({ origin:'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_CONNECTION, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet())
--- backend/src/Database/dbconnections.js
+++ backend/src/Database/dbconnections.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbconnect = async () => {
+  // check the mongo uri set or not.
+  const { MONGO_URI } = process.env;
+  if (!MONGO_URI) throw new Error("MONGO_URI is not set!");
   try {
     await mongoose.connect(process.env.MONGO_URI, { dbName: "TODO_database" });
     console.log("database is connected!");
